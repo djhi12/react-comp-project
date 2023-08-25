7e9a0bb723fbd11318ec283e3a39799f0d58b02d
djhi12 <ido20002@byui.edu>
2023-08-26 06:55:51 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className='bg-success'>Header</h1>
+      <h1 className='bg-warning'>Header</h1>
       <p>Samplpe text here.</p>
     </div>
   );
