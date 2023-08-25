a3f8ad1f6a6b00d6cda5976999cb4337c7b14a2f
djhi12 <ido20002@byui.edu>
2023-08-26 06:41:03 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Add this line
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Header</h1>
+      <h1 className='bg-success'>Header</h1>
     </div>
   );
 }
